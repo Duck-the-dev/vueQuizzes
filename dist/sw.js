f4af274e8f28ea9c77e2b319ecfcce23cb05aefb
mahmoud <mahmoud.mohamed30120@gmail.com>
2023-03-04 15:55:33 +0200

    fix(cdn link):

--- dist/sw.js
+++ dist/sw.js
@@ -78,14 +78,14 @@ define(['./workbox-148cb7e5'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/app-501b1210.js",
+    "url": "assets/app-ea3e29fe.js",
     "revision": null
   }, {
     "url": "assets/index-40ee79d0.css",
     "revision": null
   }, {
     "url": "index.html",
-    "revision": "3bb76c9cfac8c6ec095eb6582cd6da09"
+    "revision": "8416c12ef67fbc670aae097035292385"
   }, {
     "url": "registerSW.js",
     "revision": "1872c500de691dce40960bb85481de07"
