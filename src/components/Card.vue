45636ccd839a1724a69429dfc004f1ad94e9f51c
mahmoud <mahmoud.mohamed30120@gmail.com>
2023-03-04 16:57:18 +0200

    improvement:

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-const props = defineProps({
+defineProps({
   title: String,
   questions: Number,
+  img: String,
 })
 
-let imgPath =
-  'https://raw.githubusercontent.com/Duck-the-dev/vueQuizzes/main/src/assets/' +
-  props.title +
-  '.webp'
+// let imgPath =
+//   'https://raw.githubusercontent.com/Duck-the-dev/vueQuizzes/main/src/assets/' +
+//   props.title +
+//   '.webp'
 </script>
 
 <template>
   <div class="card card-compact mx-auto w-96 max-w-fit bg-base-100 text-center shadow-xl">
     <figure class="cursor-pointer">
-      <img :src="imgPath" :alt="title" />
+      <img :src="img" :alt="title" />
     </figure>
     <div class="card-body">
       <h2 class="text-2xl font-bold">{{ title }}</h2>
